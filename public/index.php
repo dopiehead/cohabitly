acab17f73e3deb564c3b62e916e0959a9f785dcc
dopiehead <ngnimitech@gmail.com>
2026-04-27 05:02:52 +0100

    update

--- public/index.php
+++ public/index.php
@@ -3,8 +3,9 @@
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-// dd(strlen($_ENV['JWT_PUBLIC_KEY']));
+if (isset($_ENV['JWT_SECRET_KEY'])) {
+    error_log('JWT KEY LENGTH: ' . strlen($_ENV['JWT_SECRET_KEY']));
+}
 
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
