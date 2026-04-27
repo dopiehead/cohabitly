fa062a6d48d34b3b48cc5d7a7582b021eb4da3b3
dopiehead <ngnimitech@gmail.com>
2026-04-27 10:44:24 +0100

    fix: remove MakerBundle from prod, use JWT_SECRET env var for HS256

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
